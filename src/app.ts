d70f16966f1e6b1d066fbef5a3aa6b6799951ebd
= <=>
2025-08-24 21:20:40 -0300

    refactor: app server setup into typescript

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,19 @@ config()
 
 import express from 'express'
 import cors from 'cors'
+import router from './router.ts'
 
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || 'localhost'
 
-const app = express()
+export const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/api', router)
 
-app.listen(PORT, () => {
+export const server = app.listen(PORT, () => {
     console.log(`Server running at http://${HOST}:${PORT}`)
 })
--- src/router.ts
+++ src/router.ts
@@ -2,27 +2,16 @@ import { Router, type Application, type Request, type Response } from "express";
 
 const router = Router()
 
-const userRouter = Router()
-
-userRouter.get('/users', (req: Request, res: Response) => {
-    res.send('List of users')
+router.get('/users', (req: Request, res: Response) => {
+    res.send({msg: 'List of users'})
 })
 
-userRouter.post('login', (req: Request, res: Response) => {
+router.post('login', (req: Request, res: Response) => {
     throw new Error('Not implemented')
 })
 
-userRouter.post('register', (req: Request, res: Response) => {
+router.post('register', (req: Request, res: Response) => {
     throw new Error('Not implemented')
 })
 
-router.use(userRouter)
-
-
-const useRouter = (app: Application) => {
-    app.use('/api', router)
-}
-
-
-
-export default { useRouter, router }
+export default router

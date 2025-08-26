661474d8754bc3e05c324358363e6e80bbad2088
= <=>
2025-08-25 21:32:27 -0300

    feat: add UserController

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,48 @@
+import type { Request, Response } from "express"
+import UserService from "../services/UserService.ts"
+
+export default class UserController {
+
+    static register(){
+        throw new Error('not implemented')
+    }
+
+    static login(){
+        throw new Error('not implemented')
+
+    }
+
+    static forgotPassword(){
+        throw new Error('not implemented')
+
+    }
+
+    static async findAllUsers(req: Request, res: Response){
+        try{
+            const users = await UserService.readUsers()
+
+            if(users){
+                return res.status(200).json(users)
+            }
+
+            
+            return res.status(200).json([])
+
+        }catch(err: any){
+            return res.status(500).json({ error: err.message })
+        }
+    }
+
+
+    static findUserById(){
+        throw new Error('not implemented')
+    }
+
+    static updateUser(){
+        throw new Error('not implemented')
+    }
+
+    static deleteUser(){
+        throw new Error('not implemented')
+    }
+}

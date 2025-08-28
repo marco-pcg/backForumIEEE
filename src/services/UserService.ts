a511d67503e48804ee87b03a7d946dfcaffc8867
= <=>
2025-08-27 21:11:17 -0300

    feat:: add method login

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt'
 import type { User } from "../models/User.ts";
 import UserRepository from "../repositories/UserRepository.ts";
+import { Prisma } from '../../generated/prisma/edge.js';
+import CustomValidationError from '../errors/CustomValidationError.ts';
 
 export default class UserService {
 
@@ -23,20 +25,57 @@ export default class UserService {
             throw new Error('invalid email address');
         }
 
-        const saltRounds = 12
-        const hashedPassword = await bcrypt.hash(user.password, saltRounds)
+        const hashedPassword = await this.hashPassword(user.password)
 
         user.password = hashedPassword
         const created = await UserRepository.create(user)
 
         return created
     }
+
     private static async hashPassword(password: string){
         const saltRounds = 12
         const hashedPassword = await bcrypt.hash(password, saltRounds)
 
         return hashedPassword
     }
+
+    static async login(email: string, password: string){
+        
+        try{
+
+            const hashedPassword = await this.hashPassword(password)
+
+            if(!email || !password){
+                throw new CustomValidationError('email and password are required')
+            }
+
+
+            const user = await UserRepository.read({
+                email,
+                password: hashedPassword
+            })
+
+            if(user){
+                return user
+            }
+
+        }catch(err: any){
+
+            if(err instanceof Prisma.PrismaClientValidationError){
+                err.message = 'invalid e-mail address or password'
+                throw err
+
+            }else if(err instanceof CustomValidationError){
+
+                throw err
+            }
+            throw new Error('error logging in user')
+
+        }
+
+    }
+
 
     static async readUsers(){
 

808ce19603550e73407a0f8da94c6ec17add8d1c
= <=>
2025-09-03 20:35:49 -0300

    feat: add auth's middleware functions

---
+++ src/utils/middlewares/auth.ts
@@ -0,0 +1,34 @@
+import type { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+// Extend Express Request interface to include 'user'
+declare global {
+    namespace Express {
+        interface Request {
+            user?: any;
+        }
+    }
+}
+
+
+export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+
+    const authHeader = req.headers['authorization']
+    
+    if (!authHeader?.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    const token = authHeader && authHeader.split(' ')[1]
+
+    try {
+
+        const payload = verify(token as string, process.env.ACCESS_TOKEN_SECRET as string) as any
+
+        req.user = payload
+        next()
+    }catch (err: any | Error) {
+        return res.status(403).json({ message: 'invalid or expired token' })
+    }
+
+}

789572a78c50b31fb70eadfdd7ee7fdac92c8890
= <=>
2025-09-03 20:33:55 -0300

    feat: add auth router

---
+++ src/routes/auth.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import AuthController from "../controllers/AuthController.ts";
+
+const router = Router()
+
+router.post('/silent-login', AuthController.silentLogin)
+
+router.post('/login', AuthController.login)
+
+router.post('/register', AuthController.register)
+
+
+
+export default router

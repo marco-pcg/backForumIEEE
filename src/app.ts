4e035cf9c8de39454d412321617c176efe1bc846
= <=>
2025-09-12 00:57:01 -0300

    refactor: add credentials within cors option

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,9 @@ const HOST = process.env.HOST || 'localhost'
 
 export const app = express()
 
-app.use(cors())
+app.use(cors({
+    credentials: true
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 

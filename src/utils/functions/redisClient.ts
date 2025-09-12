164f5456d84dfb8e91cd20f5c9c691bb472953d1
= <=>
2025-09-12 00:57:33 -0300

    feat: add redis client

---
+++ src/utils/functions/redisClient.ts
@@ -0,0 +1,9 @@
+import { createClient } from "redis";
+
+export const redis = createClient()
+
+redis.on('error', err => {
+    console.log('Redis Client Error',err.message)
+})
+
+await redis.connect()

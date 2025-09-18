319cd92f44c5ede5be9b551e3a7faefebdb4f103
= <=>
2025-09-18 00:05:46 -0300

    refactor: sign user as object

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,15 +5,15 @@ const User = require('../models/User.js')
 const router = require('express').Router()
 
 const generateTokens = user => {
-    const accessToken = jwt.sign(user, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' })
-    const refreshToken = jwt.sign(user, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' })
+    const accessToken = jwt.sign({user}, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' })
+    const refreshToken = jwt.sign({user}, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' })
     return { accessToken, refreshToken }
 }
 
 // Register route
 
 router.post('/register', async (req, res) => {
-    const { name, username, password } = req.body
+    const { name, username, password, email } = req.body
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10)
@@ -21,12 +21,18 @@ router.post('/register', async (req, res) => {
         const user = {
             name,
             username,
+            email,
             password: hashedPassword,
             role: 'user', // Default role
         }
         await User.createUser(user)        
 
-        const tokens = generateTokens(reqUser)
+        const tokens = generateTokens({
+            name: user.name,
+            username: user.username,
+            role: user.role,
+            email: user.email
+        })
 
         res.cookie('refreshToken', tokens.refreshToken, {
             httpOnly: true,
@@ -43,10 +49,10 @@ router.post('/register', async (req, res) => {
 // Login route
 
 router.post('/login', async (req, res) => {
-    const { username, password } = req.body
+    const { email, password } = req.body
     
     try {
-        const user = await User.findByUsername(username)
+        const user = await User.findByEmail(email)
         if (!user)
             return res.status(401).json({ error: 'User does not exist' })
 
@@ -55,9 +61,10 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ error: 'Invalid credentials' })
         
         const reqUser = {  
-            id: user.id,
-            role: user.role,
-            username: user.username
+            username: user.username,
+            email: user.email,
+            name: user.name,
+            role: user.role
         }
 
         const tokens = generateTokens(reqUser)
@@ -76,21 +83,24 @@ router.post('/login', async (req, res) => {
 })
 
 router.post('/refresh', (req, res) => {
-      const token = req.cookies.refreshToken;
-  if (!token) return res.status(401).send('Missing refresh token');
+    const token = req.cookies.refreshToken;
+    if (!token) return res.status(401).send('Missing refresh token');
 
-  try {
-    const payload = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
-    const tokens = generateTokens({ username: payload.username });
-    res.cookie('refreshToken', tokens.refreshToken, {
-      httpOnly: true,
-      sameSite: 'Lax',
-      secure: false,
-    });
-    res.json({ accessToken: tokens.accessToken, user: { username: payload.username } });
-  } catch {
-    res.status(401).send('Invalid token');
-  }
+    try {
+
+        const payload = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+
+        const tokens = generateTokens({ user: payload.user });
+        res.cookie('refreshToken', tokens.refreshToken, {
+        httpOnly: true,
+        sameSite: 'Lax',
+        secure: false,
+        });
+        
+        res.json({ accessToken: tokens.accessToken, user: payload.user });
+    } catch (err) {
+        res.status(401).send('Invalid token', err);
+    }
 })
 
 router.post('/logout', (req, res) => {

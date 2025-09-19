feddf73b289cda5d5506dcbf30132b274cc307a3
= <=>
2025-09-19 10:01:18 -0300

    feat: add functions to generate tokens (refresh and access)

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,15 +1,25 @@
 const jwt = require('jsonwebtoken')
 
-const authenticate = (req, res, next) => {
+exports.generateAccessToken = user => {
+    return jwt.sign({ user }, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' })
+}
+
+exports.generateTokens = user => {
+    const accessToken = jwt.sign({ user }, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' })
+    const refreshToken = jwt.sign({user}, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' })
+    return { accessToken, refreshToken }
+}
+
+exports.authenticate = (req, res, next) => {
 
     const authHeader = req.headers.authorization
     const token = authHeader && authHeader.split(' ')[1]
 
     if (!token) 
-        return res.status(401).json({ message: 'No token provided' })
+        return res.status(401).json({ message: 'No access token provided' })
     
     try{
-        const user = jwt.verify(token, process.env.JWT_SECRET)
+        const user = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
         req.user = user
         next()
     }catch {
@@ -17,14 +27,16 @@ const authenticate = (req, res, next) => {
     }
 }
 
-const requireRole = role => (req, res, next) => {
+exports.requireRole = role => (req, res, next) => {
     if (req.user.role !== role) {
         return res.status(403).json({ message: 'Forbidden' })
     }
     next()
 }
 
-module.exports = {
-    authenticate,
-    requireRole
+exports.requireRefreshToken = (req, res, next) => {
+    const token = req.cookies.refreshToken;
+    if (!token) return res.status(401).json({ message: 'Missing refresh token'});
+    req.token = token;
+    next()
 }

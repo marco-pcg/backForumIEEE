9194c3c523206a7b068eeaf23ce534b91eb52542
= <=>
2025-09-19 10:01:35 -0300

    feat: add function deleteAll()

--- src/models/User.js
+++ src/models/User.js
@@ -3,22 +3,22 @@ const { v4 } = require('uuid')
 
 const TABLE_NAME = 'users'
 
-const findByUsername = username => 
+exports.findByUsername = username => 
     db(`${TABLE_NAME}`)
         .where({ username })
         .first()
 
-const findByEmail = email => 
+exports.findByEmail = email => 
     db(`${TABLE_NAME}`)
         .where({ email })
         .first()
 
-const findByIds = ids =>
+exports.findByIds = ids =>
     db(`${TABLE_NAME}`)
         .whereIn('id', ids)
         .select('id', 'name', 'username');
 
-const findById = id => 
+exports.findById = id => 
     db(`${TABLE_NAME}`)
         .where({ id })
         .first()
@@ -33,29 +33,29 @@ const findById = id =>
             }
         })
 
-const createUser = async user => {
+exports.createUser = async user => {
 
     const id = v4()
 
     await db(`${TABLE_NAME}`)
         .insert({ id, ...user})
 
-    return findById(id)
+    return this.findById(id)
 }
 
-const deleteUser = username =>
+exports.deleteUser = username =>
     db(`${TABLE_NAME}`)
         .where({ username })
         .del()
         .then(count => count > 0)
 
-const updateUser = (username, user) =>
+exports.updateUser = (username, user) =>
     db(`${TABLE_NAME}`)
         .where({ username })
         .update(user)
         .then(count => count > 0)
 
-const findAll = () =>
+exports.findAll = () =>
     db(`${TABLE_NAME}`)
         .select('id', 'name', 'username', 'created_at', 'updated_at')
         .then(rows => rows.map(row => ({
@@ -66,13 +66,7 @@ const findAll = () =>
             updatedAt: row.updated_at
         })))
 
-module.exports = {
-    findByUsername,
-    createUser,
-    deleteUser,
-    updateUser,
-    findAll,
-    findByIds,
-    findById,
-    findByEmail
-}
+exports.deleteAll = () =>
+    db(`${TABLE_NAME}`)
+        .del()
+        .then(() => {})

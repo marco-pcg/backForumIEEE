141da42d4817931669627e2d9f049ad6fffda4f1
= <=>
2025-10-01 06:28:48 -0300

    feat: add util functions

--- src/utils/functions/jwt.ts
+++ src/utils/functions/jwt.ts
@@ -1,64 +1,23 @@
-import type { Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken'
 import { v4 as uuidv4 } from 'uuid';
 import { redis } from './redisClient.ts';
+import type { User } from '../../models/User.ts';
 
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || ''
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || ''
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || ''
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || ''
 const REFRESH_TOKEN_EXPIRES = Number(process.env.REFRESH_TOKEN_EXPIRES) || 60 * 60 * 24 * 7 // 7 days
 
 if (!ACCESS_TOKEN_SECRET || !REFRESH_TOKEN_SECRET) {
     throw new Error('JWT secrets are not defined in environment variables');
 }
 
-export function generateAccessToken(user: { id: string }, expiresIn: string = '15m') {
-  return jwt.sign({ id: user.id}, ACCESS_TOKEN_SECRET, { expiresIn } as jwt.SignOptions);
+export const generateAccessToken = (user: User) => {
+    return jwt.sign({ user }, ACCESS_TOKEN_SECRET, { expiresIn: '30m' })
 }
 
-export async function generateRefreshToken(user: { id:string }, expiresIn: string | number = '7d') {
-  
-  const tokenId = uuidv4()
-
-  const refreshToken = jwt.sign(
-    { id: user.id, tokenId },
-    REFRESH_TOKEN_SECRET,
-    { expiresIn } as jwt.SignOptions
-  )
-
-  await redis.set(tokenId, user.id.toString(), {
-    expiration: {
-      type: 'EX',
-      value: REFRESH_TOKEN_EXPIRES
-    }
-  })
-
-  return refreshToken
-}
-
-export function sendRefreshToken(res: Response, token: string) {
-  
-  res.cookie('refresh_token', token, {
-    httpOnly: true,
-    sameSite: 'lax',
-    path: '/auth/refresh',
-    secure: false,
-    maxAge: REFRESH_TOKEN_EXPIRES * 1000
-  })
-}
-
-export async function revokeRefreshToken(tokenId: string) {
-  await redis.del(tokenId)
-}
-
-export async function isRefreshTokenValid(tokenId: string): Promise<boolean> {
-  const exists = await redis.exists(tokenId)
-  return exists === 1
-}
-
-export function verifyAccessToken(token: string) {
-  return jwt.verify(token, ACCESS_TOKEN_SECRET);
-}
-
-export function verifyRefreshToken(token: string) {
-  return jwt.verify(token, REFRESH_TOKEN_SECRET);
+export const generateTokens = (user: User) => {
+    const accessToken = jwt.sign({ user }, ACCESS_TOKEN_SECRET, { expiresIn: '30m' })
+    const refreshToken = jwt.sign({user}, REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+    return { accessToken, refreshToken }
 }
--- src/utils/middlewares/auth.ts
+++ src/utils/middlewares/auth.ts
@@ -1,35 +1,46 @@
 import type { Request, Response, NextFunction } from "express";
+import { ACCESS_TOKEN_SECRET } from "../functions/jwt.ts";
 
-const verify = (await import("jsonwebtoken")).verify
+import jwt from 'jsonwebtoken'
+import type { User } from "../../models/User.ts";
 
 // Extend Express Request interface to include 'user'
 declare global {
     namespace Express {
         interface Request {
-            user?: any;
+            user: User | any;
+            token?: string;
         }
     }
 }
 
-
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
 
-    const authHeader = req.headers['authorization']
-    
-    if (!authHeader?.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'No token provided' });
-    }
-
+    const authHeader = req.headers.authorization
     const token = authHeader && authHeader.split(' ')[1]
 
-    try {
-
-        const payload = verify(token as string, process.env.ACCESS_TOKEN_SECRET as string) as any
-
-        req.user = payload
+    if (!token) 
+        return res.status(401).json({ message: 'No access token provided' })
+    
+    try{
+        const user = jwt.verify(token, ACCESS_TOKEN_SECRET)
+        req.user = user
         next()
-    }catch (err: any | Error) {
-        return res.status(403).json({ message: 'invalid or expired token' })
+    }catch {
+        return res.status(403).json({ message: 'Invalid token' })
     }
+}
 
+export const requireRole = (role: string) => (req: Request, res: Response, next: NextFunction) => {
+    if (req.user.role !== role) {
+        return res.status(403).json({ message: 'Forbidden' })
+    }
+    next()
+}
+
+export const requireRefreshToken = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.refreshToken;
+    if (!token) return res.status(401).json({ message: 'Missing refresh token'});
+    req.token = token;
+    next()
 }

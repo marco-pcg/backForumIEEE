276bb9c084ee519258d4dbad7c37befd79e0bad1
= <=>
2025-10-16 20:53:58 -0300

    feat: implements auth middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -10,6 +10,6 @@ router.post('/register', AuthController.register)
 
 router.post('/refresh', requireRefreshToken, AuthController.refreshToken)
 
-router.post('/logout', AuthController.logout)
+router.post('/logout', requireRefreshToken, AuthController.logout)
 
 export default router

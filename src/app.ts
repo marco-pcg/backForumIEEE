7bdcab9f8eeb2eec77c2bdd1b541c898e39c99e6
= <=>
2025-10-16 20:56:30 -0300

    feat/ci: add environment variables for docker purposes (mainly)

--- src/app.ts
+++ src/app.ts
@@ -7,19 +7,25 @@ import router from './router.ts'
 import type { Server } from 'http'
 import cookieParser from 'cookie-parser'
 
-const PORT = Number(process.env.PORT) || 3000
 const HOST = process.env.HOST || 'localhost'
+const PORT = Number(process.env.PORT) || 3000
+
+const FRONTEND_CLIENT_HOST = process.env.FRONTEND_CLIENT_HOST || 'localhost'
+const FRONTEND_CLIENT_PORT = Number(process.env.FRONTEND_CLIENT_PORT) || 3000
 
 export const app = express()
 
+app.use(cookieParser())
 app.use(cors({
+    origin: `http://${FRONTEND_CLIENT_HOST}:${FRONTEND_CLIENT_PORT}`,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', router)
-app.use(cookieParser())
 
 export const listen = (port: number = PORT) => {
     return app.listen(port, () => {
